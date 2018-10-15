31639425ce482717396e2ed179d2957bc1ee6125
jeremy woo <jeremy.woo@akqa.com>
2018-10-16 07:32:22 +1300

    updated build and sql run command.

--- mssql/docker_run.cmd
+++ mssql/docker_run.cmd
@@ -1,8 +1,1 @@
-
-SET DOCKER_IMAGE_NAME=10.38.11.73:5000/bunnings/mssql:v1
-
-SET DBPATH=%1
-IF "%1"=="" SET DBPATH = "C:\mssqldata"
-if not exist %DBPATH% mkdir %DBPATH%
-
-docker run -d  -m 2048M  -p 1400:1433 -v C:/mssqldata/:C:/mssqldata/ -e sa_password=dtd99!! -e restore_dbs="[{'dbName':'ProductCatalog','DBLogicalName':'Bunnings.CommerceServer.ProductCatalog', 'Bakfilename':'C:\\mssqldata\\ProductCatalog.bak'}]" %DOCKER_IMAGE_NAME%
+docker run -d -p 1400:1433 -v C:/mssqldata/:C:/mssqldata/ -e restore_dbs="[{'dbName':'ProductCatalog','DBLogicalName':'Bunnings.CommerceServer.ProductCatalog', 'Bakfilename':'C:\\mssqldata\\ProductCatalog.bak'}]" -e targetRestorePath=C:\MSSQLDATA\DATA\ mssql:v1
